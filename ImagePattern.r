6abffe29d5e1b60bfd3f7e87ccabcf3bf2656e97
tonywe1 <tong.nicholwei@gmail.com>
2012-07-06 11:04:49 +0800

    change some comments

--- ImagePattern.r
+++ ImagePattern.r
@@ -1,24 +1,21 @@
 ## graphical function
-## for automated pattern recognition based on pixel densities
-## using R/EBImage pkg
+## for automated pattern recognition using R/biocondcutor "EBImage" pkg
 
 ## adjustable parameters: thresh(w, h, offset), makeBrush(size), 
 ## small spots with less pixels (< 100 here)
 
-setwd("d:/eBook/working/")
 library(EBImage)
 
 # read in images
-files <- dir(pattern = "sample.tif$")
+setwd("d:/working/")
+files <- "sample.tif"
 pic <- readImage(files)
 pic.blue <- channel(pic, mode = "blue")  # extract corresponding channel
 
 # filter 1: remove background noise
 tmp <- thresh(pic.blue, w = 100, h = 100, offset = 0.2)  
-  # for clear pattern, use bigger "window" and higher "offset"
 # filter 2: modify the morphological shape
 kern <- makeBrush(5, shape = "disc", step = F)
-  # bigger "size" give more significant modification
 tmp <- dilate(erode(tmp, kern), kern)
 pic.lab <- bwlabel(tmp)
 # filter 3: remove morphological shape with less pixels
@@ -33,7 +30,7 @@ for (i in mt) {
 imageData(pic.lab) <- tmp2
 
 pic.lab <- fillHull(pic.lab)
-  # max(pic.lab) is the count of targets
+  # max(pic.lab) is the count of nuclei
 # output the labelled image
 pic.out <- paintObjects(pic.lab, pic, col = c('yellow', NA))
 xy <- hullFeatures(pic.lab)[, c('g.x', 'g.y')]
