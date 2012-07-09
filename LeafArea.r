cabea3825517aa104652af19ebb39ae239a4a312
tonyworld <tong.nicholwei@gmail.com>
2012-07-09 19:36:19 +0800

    first release of LeafArea

---
+++ LeafArea.r
@@ -0,0 +1,38 @@
+## graphical application of EBImage
+## for calculate an area ratio of leaf blades
+
+
+library(EBImage)
+
+# read the image
+setwd("D:/working/")
+pic <- readImage("sample.jpg")
+
+# extract the 'blue' channel
+pic.data <- imageData(channel(pic, mode = "blue"))
+pic.data <- 1 - pic.data  # reverse the image 
+hist(pic.data)
+
+# noticed that the background is around 0.4-0.55, while the leaf is about 0.9-1
+pic.data[pic.data < .8] <- 0
+pic.data[pic.data >= .8] <- 1
+pic.lab <- bwlabel(pic.data)  # attempt to enclose the holes
+pic.lab <- fillHull(pic.lab)  
+kern <- makeBrush(3, shape = "disc", step = F)
+pic.lab <- erode(pic.lab, kern)  # remove only a few noisy pixels
+
+# calculate the area ratio
+pic.shape <- computeFeatures.shape(pic.lab)
+leaf.area <- pic.shape[, "s.area"]
+area.ratio <- leaf.area[1] / leaf.area[2]
+cat("The ratio of leaf areas is ", round(area.ratio, digits = 3), ".\n", 
+    sep = "")
+
+# layout the resulting image
+pic.out <- paintObjects(pic.lab, pic, opac = c(NA, 0.45), col = c(NA, "red"))
+xy <- computeFeatures.moment(pic.lab, pic)[, c("m.cx", "m.cy")]
+font <- drawfont(weight = 600, size = 35)
+pic.out <- drawtext(pic.out, xy = xy, 
+                    labels = as.character(leaf.area), 
+                    font = font, col = "white")
+writeImage(pic.out, "sample.out.jpg")
